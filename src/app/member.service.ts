20358456188d542b99f1f898af0da42ecef30f7f
Michael Dunlap <michaeldunlap@earthlink.net>
2017-08-25 16:52:32 -0700

    app will allow admin to update bio

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -20,7 +20,12 @@ export class MemberService {
   }
 
   getMemberById(memberId: string) {
-    return this.database.object('members/' + memberId);
+    return this.database.object('/members/' + memberId);
+  }
+
+  updateMember(localUpdatedMember) {
+    var memberEntryInFirebase = this.getMemberById(localUpdatedMember.$key);
+    memberEntryInFirebase.update({name: localUpdatedMember.name, instrument: localUpdatedMember.instrument, bio: localUpdatedMember.bio, imageUrl: localUpdatedMember.imageUrl});
   }
 
 
--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -13,11 +13,13 @@ import { FirebaseListObservable } from 'angularfire2/database';
 export class RosterComponent implements OnInit {
 
   members: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private memberService: MemberService) { }
 
   ngOnInit() {
     this.members = this.memberService.getMembers();
+    console.log(this.router.url);
   }
 
  goToDetailPage(clickedMember) {
