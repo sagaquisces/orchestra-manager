ad550e8e8a92ca7f037ec949c81f36e8b98cb310
Michael Dunlap <michaeldunlap@earthlink.net>
2017-08-25 13:34:54 -0700

    add a member service and appropriate pathway to inject into the roster component

---
+++ src/app/mock-members.ts
@@ -0,0 +1,16 @@
+import { Member } from './member.model'
+
+export const MEMBERS: Member[] = [
+ new Member("Mac Trier", "Flute",
+     "Bio will go here", "https://images.pexels.com/photos/262391/pexels-photo-262391.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 1),
+ new Member("Dominic Smith", "Trumpet",
+     "Bio will go here", "https://images.pexels.com/photos/544716/pexels-photo-544716.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 2),
+ new Member("Michael Schultz", "Trombone",
+     "Bio will go here", "https://images.pexels.com/photos/450214/pexels-photo-450214.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 3),
+ new Member("Philip Exultate", "Percussion",
+     "Bio will go here", "https://images.pexels.com/photos/428339/pexels-photo-428339.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 4),
+ new Member("Nancita Trifonov", "Violin",
+     "Bio will go here", "https://images.pexels.com/photos/458665/pexels-photo-458665.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 5),
+ new Member("Julie Meyer", "Viola",
+     "Bio will go here", "https://images.pexels.com/photos/415298/pexels-photo-415298.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 6)
+];
--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -1,33 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member.model';
 import { Router } from "@angular/router";
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-roster',
   templateUrl: './roster.component.html',
-  styleUrls: ['./roster.component.css']
+  styleUrls: ['./roster.component.css'],
+  providers: [MemberService]
 })
 export class RosterComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  members: Member[];
+
+  constructor(private router: Router, private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
-
-  members: Member[] = [
-   new Member("Mac Trier", "Flute",
-       "Bio will go here", "https://images.pexels.com/photos/262391/pexels-photo-262391.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 1),
-   new Member("Dominic Smith", "Trumpet",
-       "Bio will go here", "https://images.pexels.com/photos/544716/pexels-photo-544716.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 2),
-   new Member("Michael Schultz", "Trombone",
-       "Bio will go here", "https://images.pexels.com/photos/450214/pexels-photo-450214.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 3),
-   new Member("Philip Exultate", "Percussion",
-       "Bio will go here", "https://images.pexels.com/photos/428339/pexels-photo-428339.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 4),
-   new Member("Nancita Trifonov", "Violin",
-       "Bio will go here", "https://images.pexels.com/photos/458665/pexels-photo-458665.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 5),
-   new Member("Julie Meyer", "Viola",
-       "Bio will go here", "https://images.pexels.com/photos/415298/pexels-photo-415298.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 6)
- ];
 
  goToDetailPage(clickedMember: Member) {
    this.router.navigate(['members', clickedMember.id]);
